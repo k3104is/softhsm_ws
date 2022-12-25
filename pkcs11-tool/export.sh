9119a832c644b8bb3e86d14b75d5f946cd5df701
Satoshi Kataoka <k.3104is@gmail.com>
2022-12-26 00:29:58 +0900

    add memo

--- pkcs11-tool/export.sh
+++ pkcs11-tool/export.sh
@@ -4,3 +4,7 @@
 export PKCS11_MODULE=$(find /usr/lib/ -name p11-kit-proxy.so)
 export SO_PIN=7635005489180126
 export USR_PIN=12345678
+
+
+# export SOFTHSM2_CONF=/usr/share/softhsm/softhsm2.conf
+# directories.tokendir = /var/lib/softhsm/tokens/
